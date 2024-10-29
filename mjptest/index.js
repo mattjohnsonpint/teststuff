1b978c34a27e193738126be770cfabcded586a96
Matt Johnson-Pint <mjp@hypermode.com>
2024-10-28 23:01:29 -0700

    .

--- mjptest/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { isatty } from "node:tty";
-import process from "node:process";
-
-console.log("stdin", isatty(process.stdin.fd));
-console.log("stdout", isatty(process.stdout.fd));
-console.log("stderr", isatty(process.stderr.fd));
